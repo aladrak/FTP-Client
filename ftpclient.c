7f1a3670f9b56f49a56bc565e5e2fbfa0c2eb742
aladrak <114636165+aladrak@users.noreply.github.com>
2023-05-15 22:34:57 +0700

    Add files via upload

---
+++ ftpclient.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#define PACKET_MAX_SIZE 512
+
+void getConn(SOCKET *s, char *ipAddr, int port) {
+    // Создание сокета
+    if ((*s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Failed to create listening socket. Error %d\n", WSAGetLastError());
+        exit(0);
+    }
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET; // тип адреса (TCP/IP)
+    //адрес сервера. Т.к. TCP/IP представляет адреса в числовом виде, то для перевода
+    // адреса используем функцию inet_addr.
+    serverAddress.sin_addr.s_addr = inet_addr(ipAddr);
+    // Порт. Используем функцию htons для перевода номера порта из обычного в //TCP/IP представление.
+    serverAddress.sin_port = htons(port);
+
+   // Выполняем соединение с сервером:
+    if (connect(*s, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        printf("Connection error %d.\n", WSAGetLastError());
+        closesocket(*s);
+        exit(0);
+    }
+    printf("Connection successfully!\n");
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET serverSocket;
+
+    // Инициализация Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("Error %d during initialization Winsock.\n", WSAGetLastError());
+        return 1;
+    }
+
+    // 
+    getConn(&serverSocket, "127.0.0.2", 8080);
+
+    char resp[] = "s";
+    char msgbuff[PACKET_MAX_SIZE];
+
+    while (1) {
+
+        // Получение сообщения
+        int len; 
+        do {
+            if ((len = recv(serverSocket, (char*)&msgbuff, PACKET_MAX_SIZE, 0)) == SOCKET_ERROR) {
+                return -1;
+            }
+            for (int i = 0; i < len; i++) {
+                printf ("%c", msgbuff[i]);
+            }
+            printf("\n");
+            break;
+        } while (len != 1);
+
+        // Отправка данных
+        if (send(serverSocket, (char*)&resp, sizeof(resp) - 1, 0) == SOCKET_ERROR) {
+            printf("Sending error %d.\n", WSAGetLastError());
+            closesocket(serverSocket);
+            return 1;
+        }
+        // send(serverSocket, "-1\n", 3, 0);
+    }
+
+    printf("Connection lost.\n");
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
